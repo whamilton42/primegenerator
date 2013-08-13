05d2246442269248d2ca0917558fcf5cdd40c6af
Will Hamilton <whamilton42@hotmail.com>
2013-08-13 21:16:34 +0100

    Printer.multiplication_table_string

--- lib/printer.rb
+++ lib/printer.rb
@@ -6,4 +6,20 @@ class Printer
 		@primes = args.fetch(:prime_number_generator).call(@number_of_primes)
 	end
 
+	def multiplication_table_string
+		string = "\t"
+		@primes.each do |prime|
+			string << "#{prime}\t"
+		end
+		string << "\n"
+		@primes.each do |prime|
+			string << "#{prime}\t"
+			@primes.each do |other_prime|
+				string << "#{prime * other_prime}\t"
+			end
+			string << "\n"
+		end
+		string
+	end
+
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -7,4 +7,19 @@ describe Printer do
 		printer = Printer.new(number_of_primes: 10, prime_number_generator: Proc.new{primes})
 		expect(printer.primes).to eql(primes)
 	end
+
+	describe "#multiplication_table_string" do	
+		it "should include all of the primes at least twice" do
+			primes = [1,2,3,999]
+			printer = Printer.new(number_of_primes: 10, prime_number_generator: Proc.new{primes})
+			primes.each { |prime| expect(printer.multiplication_table_string.scan(prime.to_s).length).to be >= 2 }
+		end
+
+		it "should include at least one of the multiplied results we're expecting" do
+			primes = [1,2,3,999]
+			expected_multiplied_result = primes[-2] * primes[-1]
+			printer = Printer.new(number_of_primes: 10, prime_number_generator: Proc.new{primes})
+			expect(printer.multiplication_table_string.scan(expected_multiplied_result.to_s).length).to be >= 1
+		end
+	end
 end
