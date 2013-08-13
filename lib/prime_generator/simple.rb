57ff08d66dd14b3cd98363977b6a0c5943d89270
Will Hamilton <whamilton42@hotmail.com>
2013-08-13 23:46:46 +0100

    Convert simple generator to enumerator.

--- lib/prime_generator/simple.rb
+++ lib/prime_generator/simple.rb
@@ -1,5 +1,15 @@
 require_relative '../prime_generator'
-class PrimeGenerator::Simple
+class PrimeGenerator::Simple < Enumerator
+	def initialize
+		super do |yielder|
+			candidate = 2
+			loop do
+				yielder << candidate if self.class.is_prime?(candidate)
+				candidate += 1
+			end
+		end
+	end
+
 	class << self
 		def is_prime?(number)
 			return false if number == 1
@@ -10,13 +20,7 @@ class PrimeGenerator::Simple
 		end
 
 		def first_n_primes(n)
-			primes = []
-			candidate = 1
-			while primes.length < n
-				primes << candidate if is_prime?(candidate)
-				candidate += 1
-			end
-			primes
+			new.take(n)
 		end
 
 		def nth_prime(n)
