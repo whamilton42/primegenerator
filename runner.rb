26268f8ba7d867d6a748321093182e2838a87812
Will Hamilton <whamilton42@hotmail.com>
2013-08-13 21:43:28 +0100

    Loop through generators in runner

--- runner.rb
+++ runner.rb
@@ -1,7 +1,17 @@
-require_relative 'lib/printer'
+require_relative 'lib/prime_multiplication_table_printer'
 require_relative 'lib/prime_generator/simple'
 
-number_of_primes = 10
-printer = Printer.new(number_of_primes: number_of_primes, prime_number_generator: PrimeGenerator::Simple.method(:first_n_primes))
-puts "Multiplication table of first #{number_of_primes} using a very simple generation method"
-printer.output(outputter: Proc.new{|output| puts output })
+generators = [PrimeGenerator::Simple]
+
+generators.each do |generator|
+	puts "Using #{generator} generator"
+
+	number_of_primes = 10
+	printer = PrimeMultiplicationTablePrinter.new(number_of_primes: number_of_primes, prime_number_generator: generator.method(:first_n_primes))
+	puts "Multiplication table of first #{number_of_primes} primes"
+	printer.output(outputter: Proc.new{|output| puts output })
+
+	n = 1000
+	nth_prime = generator.nth_prime(n)
+	puts "Calculation of #{n}th prime: #{nth_prime}"
+end
