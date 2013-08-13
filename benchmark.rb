b4c560bc9bb5e4026e476524cf254f27b316c98b
Will Hamilton <whamilton42@hotmail.com>
2013-08-14 00:06:37 +0100

    Benchmark label width

--- benchmark.rb
+++ benchmark.rb
@@ -3,14 +3,15 @@ require_relative 'lib/prime_generator/simple'
 require_relative 'lib/prime_generator/erastothenes'
 
 generators = [PrimeGenerator::Simple, PrimeGenerator::Erastothenes]
+label_width = generators.map { |generator| generator.to_s.length }.max + 15
 
-Benchmark.bm(3) do |x|
+Benchmark.bm(label_width) do |x|
 	generators.each do |generator|
 		x.report("take(10) -- #{generator}") { generator.new.take(10) }
 	end
 end
 
-Benchmark.bm(3) do |x|
+Benchmark.bm(label_width) do |x|
 	generators.each do |generator|
 		x.report("take(500) -- #{generator}") { generator.new.take(500) }
 	end
