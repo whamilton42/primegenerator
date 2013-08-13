fd95b7a5cd02caf55f66c251437c17b9d7bfadd7
Will Hamilton <whamilton42@hotmail.com>
2013-08-14 00:17:08 +0100

    Fixes for runner - use new enumerable interface.

--- runner.rb
+++ runner.rb
@@ -1,17 +1,17 @@
 require_relative 'lib/prime_multiplication_table_printer'
 require_relative 'lib/prime_generator/simple'
 
-generators = [PrimeGenerator::Simple]
+generators = [PrimeGenerator::Simple.new]
 
 generators.each do |generator|
 	puts "Using #{generator} generator"
 
 	number_of_primes = 10
-	printer = PrimeMultiplicationTablePrinter.new(number_of_primes: number_of_primes, prime_number_generator: generator.method(:first_n_primes))
+	printer = PrimeMultiplicationTablePrinter.new(number_of_primes: number_of_primes, prime_number_generator: generator.method(:take))
 	puts "Multiplication table of first #{number_of_primes} primes"
 	printer.output(outputter: Proc.new{|output| puts output })
 
 	n = 1000
-	nth_prime = generator.nth_prime(n)
+	nth_prime = generator.nth(n)
 	puts "Calculation of #{n}th prime: #{nth_prime}"
 end
