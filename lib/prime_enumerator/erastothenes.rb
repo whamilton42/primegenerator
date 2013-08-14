5b5aaf7d2656a709eaf7aea3cc956e9d14b36585
Will Hamilton <whamilton42@hotmail.com>
2013-08-14 23:51:48 +0100

    Remove un-needed use of instance variables.

--- lib/prime_enumerator/erastothenes.rb
+++ lib/prime_enumerator/erastothenes.rb
@@ -1,35 +1,34 @@
 require_relative '../prime_enumerator'
 class PrimeEnumerator::Erastothenes < PrimeEnumerator
-  attr_reader :upper_bound, :candidates, :prime
 
   def initialize(args)
-    @upper_bound = args.fetch(:upper_bound)
-    @candidates = (0..@upper_bound).collect { |num| true }
-    @prime = 2
+    upper_bound = args.fetch(:upper_bound)
+   	candidates = (0..upper_bound).collect { |num| true }
+    prime = 2
 
     super() do |yielder|
       loop do
 
-        while @prime*prime <= @upper_bound
-          if @candidates[@prime]
-            yielder << @prime
+        while prime*prime <= upper_bound
+          if candidates[prime]
+            yielder << prime
 
-            eliminate = @prime*@prime
-            if eliminate > @upper_bound
+            eliminate = prime*prime
+            if eliminate > upper_bound
               raise StopIteration
             else
-              while eliminate < @upper_bound
-                @candidates[eliminate] = false
-                eliminate += @prime
+              while eliminate < upper_bound
+                candidates[eliminate] = false
+                eliminate += prime
               end
             end
           end
 
-          @prime += 1
+          prime += 1
         end
 
         # If we're beyond the upper bound, we can safely assume that all of the remaining numbers are primes.
-        (2...@upper_bound).find_all { |i| i > @prime and @candidates[i] }.each { |prime| yielder << prime }
+        (2...upper_bound).find_all { |i| i > prime and candidates[i] }.each { |prime| yielder << prime }
       end
     end
   end
